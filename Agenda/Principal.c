b76928c121bd16ce04d7f6c7c2fa4529d50705ec
Lucas Lopes Pereira <44032349+LucasPLopes@users.noreply.github.com>
2018-10-22 22:14:16 -0300

    Update Principal.c

--- Agenda/Principal.c
+++ Agenda/Principal.c
@@ -1,206 +1,217 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include "Agenda.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "Agenda.h" //inclui os Protótipos
+#define tam1 101
+#define tam2 15
 
 
-int main(){
-	
-	Lista * li;
-	
-	li = cria_lista();
-	FILE *fp;
-	fp= fopen("contatos.txt","r");
-	
-	if(!fp)
-	{
-		printf("Erro na abertura do arquivo\n");
-		
-	}
-	
-	
-		
-	char temp[105];
-	int cont =1;
-	fflush(fp);
-	struct contato cc;
-    while (fgets(temp, sizeof(temp), fp))
-	{
-        if( cont == 1)
-		{
-			
-			strcpy(cc.nome, temp);
-			cont++;
-			
-		}
-		else if( cont == 2)
-		{
-			char temp2[105];
-			strcpy(temp2, temp);
-			strncpy(cc.telefone, temp2,10);
-			cont++;
-			
-		}else if( cont == 3)
-		{
-			
-			strcpy(cc.endereco, temp);
-			cont++;
-		}
-		else if( cont == 4)
-		{
-			cc.cep = atoi(temp);
-			cont++;
-		}else if( cont == 5)
-		{
+int limite_comparador = 15;
 
-			strcpy(cc.nascimento, temp);
-			cont++;
-		}else
-		{cont = 1;
-			int c ; 
-			c =strlen(cc.nome);
-			cc.nome[c -1] =' ';
-			c =strlen(cc.nascimento);
-			cc.nascimento[c -1] =' ';
-			c =strlen(cc.endereco);
-			cc.endereco[c -1] =' ';
-			imprime_dados(cc);
-			insere_lista_ordenada(li, cc);
-		
-		}
-		
+//Definição do tipo lista
+struct elemento{
+    struct elemento *ant;
+    struct contato dados;
+    struct elemento *prox;
+};
+typedef struct elemento Elem;
+
+Lista* cria_lista(){
+    Lista* li = (Lista*) malloc(sizeof(Lista));
+    if(li != NULL)
+        *li = NULL;
+    return li;
+}
+
+void libera_lista(Lista* li){
+    if(li != NULL){
+        Elem* no;
+        while((*li) != NULL){
+            no = *li;
+            *li = (*li)->prox;
+            free(no);
+        }
+        free(li);
     }
-		
-	fclose(fp);
+}
 
-	int op;
-	// system("clear||cls ");
-	
-	MENU:
-	DEFAULT:
-	menu();
-	fflush(stdin);
-	scanf("%i",&op);
-	
-	switch (op)
+int consulta_lista_nome(Lista* li, char nome[tam1], struct contato *c ){
+    if(li == NULL)
+        return 0;
+    Elem *no = *li;
+    while(no != NULL && (strstr(no->dados.nome, nome) != NULL))
 	{
-		struct contato c;
-		case 1:
-		{
-			printf("Nome: ");
-			fflush(stdin);
-			gets(c.nome);
-			
-			
-			fflush(stdin);
-			printf("Telefone (xxxxx-xxxx): ");
-			fflush(stdin);
-			gets(c.telefone);
-			
-			
-			fflush(stdin);
-			printf("Endereco: ");
-			fflush(stdin);
-			gets(c.endereco);
-			
-			
-			printf("CEP: ");
-			fflush(stdin);
-			scanf("%d",&c.cep);
-			
-			
-			printf("Nascimento (dd/mm/aaaa): ");
-			fflush(stdin);
-			gets(c.nascimento);
-			
-		
-			insere_lista_ordenada(li,c );
-			
-			goto MENU;
-		}
-		break;
-		
-		case 2:
-		{
-			limpa_tela();
-			visualizar_lista(li);
-			goto MENU;
-		}
-		break;
-		case 3:
-		{
-			limpa_tela();
-			printf("%i Contatos cadastrados\n",tamanho_lista(li));
-			goto MENU;
-		}break;
-		case 4:
-		{
-			char str[100];
-			printf("Buscar: ");
-			fflush(stdin);
-			gets(str);
-			visualizar_contato(li, str);
-				goto MENU;	
-		}break;
-		
-		case 5: 
-		{
-			printf("Remover: ");
-			char str[101];
-			fflush(stdin);
-			fscanf(stdin, "%[^\n]s",str);
-			while(1)
-			{
-				static int num = 1;
-				if(remove_lista(li,str)==1)
-				{
-				printf("[%i]contato removido!\n",num);
-					++num;
-				}
-				else
-				{
-					printf("contato nao removido!\n");
-					break;
-				}
-			}
-			
-			goto MENU;
-		}
-		break;
-		
-		case 0:
-		{
-			
-			
-			
-			
-			{
-				FILE * fp ;
-					fp= fopen("contatos.txt","w");
-					if(!fp)
-					{
-					printf("Erro na abertura do arquivo, fim de programa\n");
-					exit(1);
-					}
-				
-				grava_lista_arq(fp,li);
-					
-				libera_lista(li);
-				fclose(fp);
-			}
-			printf("fim de programa!!\n");
-			return 0;
+        no = no->prox;
+    }
+    if(no == NULL)
+        return 0;
+    else{
+        *c  = no->dados;
+        return 1;
+    }
+}
 
-		}	
 
-			default: {
-				limpa_tela();
-				printf("opcao invalida!!");
-				goto DEFAULT;
-			}
+int insere_lista_ordenada(Lista* li, struct contato c ){
+    if(li == NULL)
+        return 0;
+    Elem *no = (Elem*) malloc(sizeof(Elem));
+    if(no == NULL)
+        return 0;
+    no->dados = c ;
+    if((*li) == NULL){//lista vazia: insere início
+        no->prox = NULL;
+        no->ant = NULL;
+        *li = no;
+        return 1;
+    }
+    else{
+        Elem *ante, *atual = *li;
+        while(atual != NULL && strncmp(atual->dados.nome, c.nome,limite_comparador)< 0){
+            ante = atual;
+            atual = atual->prox;
+        }
+        if(atual == *li){//insere início
+            no->ant = NULL;
+            (*li)->ant = no;
+            no->prox = (*li);
+            *li = no;
+        }else{
+            no->prox = ante->prox;
+            no->ant = ante;
+            ante->prox = no;
+            if(atual != NULL)
+                atual->ant = no;
+        }
+        return 1;
+    }
+}
+
+int remove_lista(Lista *li, char nome[101]){
+	
+	if((li)==NULL)
+		return 0;
+	if((*li)==NULL)//lista vazia
+		return 0;
+	Elem *no = *li;
+	while(no != NULL &&(strstr(no->dados.nome, nome)== NULL)){
+		no = no->prox;
 	}
 	
-	return 0;
+	if(no == NULL)
+		return 0;
+	
+	if(no->ant == NULL)
+		*li=no->prox;
+	else
+		no->ant->prox = no->prox;
+	if(no->prox != NULL)//não é o último
+		no->prox->ant = no->ant;
+		
+	free(no);
+	return 1;
+}
+
+
+
+int tamanho_lista(Lista* li){
+    if(li == NULL)
+        return 0;
+    int cont = 0;
+    Elem* no = *li;
+    while(no != NULL){
+        cont++;
+        no = no->prox;
+    }
+    return cont;
+}
+
+void visualizar_lista(Lista* li){
+    if(li == NULL)
+        return;
+    Elem* no = *li;
+    while(no != NULL){
+        printf("Nome: %s\n",no->dados.nome);
+        
+        no = no->prox;
+    }
+}
+
+void imprime_dados(struct contato c )
+{
+	printf("Nome: %s\n", c.nome);
+	printf("Telefone: %s\n",c.telefone);
+	printf("Endereco: %s\n", c.endereco);
+	printf("CEP: %i\n", abs(c.cep));
+	char nasc[10] = {};
+	printf("Nascimento: %c%c%c%c%c%c%c%c%c%c\n", c.nascimento[0],c.nascimento[1],'/',c.nascimento[3],c.nascimento[4],'/',c.nascimento[6],c.nascimento[7],c.nascimento[8],c.nascimento[9]);
+	printf("$\n");
+	
+	
 	
 }
 
+void visualizar_contato(Lista* li, char nome[tam1]){
+    limpa_tela();
+	if(li == NULL)
+        return;
+    Elem* no = *li;
+    while(no != NULL){
+		if(strstr(no->dados.nome,nome)!=NULL)
+			imprime_dados(no->dados);        
+        no = no->prox;
+		
+    }
+}
+
+struct contato recebe_contato(char nome[tam1], char telefone[tam2], char endereco[tam1], int cep, char nascimento[tam2])
+{
+	struct contato c;
+	strcpy(c.nome, nome);
+	strcpy(c.telefone, telefone);
+	strcpy(c.endereco, endereco);
+	c.cep = cep;// int 
+	strcpy(c.nascimento, nascimento);
+	
+	return c;
+}
+
+void menu()
+{
+	printf("\t\tAgenda de contatos\n");
+	printf("\t\tMenu\n");
+	printf("1 -Inserir novo contato\n");
+	printf("2 -Listar contatos\n");
+	printf("3 -Tamanho\n");
+	printf("4 -Buscar contato\n");
+	printf("5 -Remover contato\n");
+		
+	printf("0 -Sair\n");
+	
+	printf("Opcao: ");
+	
+}
+
+
+void grava_lista_arq(FILE *fp, Lista*li)
+{
+    if(li == NULL)
+        return;
+    Elem* no = *li;
+    while(no != NULL)
+	{
+		fflush(fp);
+		fflush(stdout);
+		
+        fprintf(fp,"%s\n%s\n%s\n%i\n%10s\n$\n",
+		(no)->dados.nome,(no)->dados.telefone, (no)->dados.endereco, (no)->dados.cep,(no)->dados.nascimento);	
+        no = no->prox;
+    
+	}
+	printf("Lista gravada!");
+}
+
+void limpa_tela(){
+					system("cls||clear");
+}
